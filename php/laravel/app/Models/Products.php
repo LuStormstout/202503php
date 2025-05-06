92162dc58cee5665f04a820bca847367f89d65ff
Lu Stormstout <lustormstout@gmail.com>
2025-05-06 22:42:58 +0900

    Eloquent ORM

--- php/laravel/app/Models/Products.php
+++ php/laravel/app/Models/Products.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  *
@@ -33,7 +34,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class Products extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
 
     public function category(): BelongsTo
     {

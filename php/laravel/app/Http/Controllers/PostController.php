416b7627883419b27232241559d6b7fc44c29ea4
Lu Stormstout <lustormstout@gmail.com>
2025-05-08 22:50:24 +0900

    表单处理、验证与 CSRF

--- php/laravel/app/Http/Controllers/PostController.php
+++ php/laravel/app/Http/Controllers/PostController.php
@@ -2,35 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the posts.
+     *
+     * @param Request $request
+     * @return View
      */
-    public function index(): View
+    public function index(Request $request): View
     {
-        $posts = Post::with('author')->paginate($this->perPage);
+        if ($request->get('search')) {
+            $posts = Post::with('author')
+                ->where('title', 'like', '%' . $request->get('search') . '%')
+                ->orWhere('content', 'like', '%' . $request->get('search') . '%')
+                ->paginate($this->perPage);
+        } else {
+            $posts = Post::with('author')->paginate($this->perPage);
+        }
+
         return view('posts.index', compact('posts'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('posts.create', [
+            'authors' => Author::all(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        // 因为我们目前没有做作者登录这个功能, 所以我们这里随机拿一个作者 id 给当前要创建的这个文章
+        if (!$request->has('author_id')) {
+            $request->merge(['author_id' => Author::pluck('id')->random()]);
+        }
+
+        // 验证请求数据
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'author_id' => 'required|exists:authors,id',
+        ]);
+
+        // 创建新的文章
+        Post::create($request->only('title', 'content', 'author_id'));
+
+        // 重定向到文章列表页面, 并显示成功消息
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
     }
 
     /**
